78d4a6260433a0264505a263855c0fcf35805fb4
chengmic <chengmic@oregonstate.edu>
2023-05-17 15:26:33 -0700

    added sample request

---
+++ sample_request.py
@@ -0,0 +1,14 @@
+import requests
+
+def make_request(param1, param2):
+    url = f'http://127.0.0.1:5000/average_salary?job_title={param1}&location={param2}'
+    response = requests.get(url)
+    data = response.json()
+
+    print (data['average_salary'])
+
+
+job='software developer'
+location = 'san diego'
+
+make_request(job, location)
